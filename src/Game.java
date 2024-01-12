46174cad0940d87ec049fa78d7a8a86330ab83ed
DELL <raghadfrh11@gmail.com>
2024-01-12 22:30:41 +0300

    raghad

--- src/Game.java
+++ src/Game.java
@@ -15,6 +15,13 @@ public class Game {
         this.player1 = new Player("EMPLOYEE", board, 1);
         this.player2 = new Player("THE Arsis", board, 2);
     }
+    public Game(String playerName1, String playerName2) {
+        this.turn = true;
+        this.scanner = new Scanner(System.in);
+        this.board = new Board(69, 7);
+        this.player1 = new Player(playerName1, board, 1);
+        this.player2 = new Player(playerName2, board, 2);
+    }
 
     public void run() {
         while (true) {
@@ -127,4 +134,143 @@ public class Game {
         boolean x = Rules.kill(board, availableRocks[rockNumber - 1], otherPlayer.getPlayRocks());
         board.printBoard();
     }
+
+    private void handleDiceRollAndMoveForComputer(Player currentPlayer, Player otherPlayer, DiceRolls rand) {
+        rand.printState();
+        boolean notAllRocksOutBoard = false;
+
+        for (PlayRock rock : otherPlayer.getPlayRocks()) {
+
+            if (rock.getPosition() != -1) {
+                System.out.println(rock.getPosition());
+                notAllRocksOutBoard = true;
+                break;
+            }
+
+        }
+        if (!notAllRocksOutBoard) {
+            for (int j = 0; j <= 1; j++) {
+                if (rand.countOnesAndNameState() != "Dest" && rand.countOnesAndNameState() != "Bunja") {
+                    rand.rollDice();
+                    rand.printState();
+                }
+            }
+            if (rand.countOnesAndNameState() != "Dest" && rand.countOnesAndNameState() != "Bunja") {
+                return;
+            }
+        }
+        System.out.println("Computer rolled a :");
+        rand.printState();
+        PlayRock[] availableRocks = otherPlayer.getPlayRocks();
+
+
+        int rockNumber;
+
+        String diceResult = rand.countOnesAndNameState();
+        if (diceResult.equals("Dest") || diceResult.equals("Bunja")) {
+            ////////////////////////
+
+            System.out.println("Sahozy : Add a new rock to the board\n2. Move an available rock 1 position");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                System.out.println("Sahozy : a new playRock will be added for : " + otherPlayer.getName());
+                for (int i = 0; i < availableRocks.length; i++) {
+                    if (availableRocks[i].getPosition() == -1) {
+                        Move.Uncle(availableRocks[i], board, choice);
+                        break;
+                    }
+                }
+            } else if (choice == 2) {
+                System.out.println("Sahzoy : choose playRock to move by 1");
+                for (int i = 0; i < availableRocks.length; i++) {
+                    if (availableRocks[i].getPosition() != -1) {
+                        System.out.println("Rock " + (i + 1) + ": Position " + availableRocks[i]);
+                    }
+                }
+
+                rockNumber = scanner.nextInt();
+                Move.Uncle(availableRocks[rockNumber - 1], board, choice);
+            }
+            //patata
+            System.out.println("sahozy : Choose a rock number to move:");
+            for (int i = 0; i < availableRocks.length; i++) {
+                if (!availableRocks[i].finish && availableRocks[i].getPosition() != -1) {
+                    System.out.println("Rock " + (i + 1) + ": Position " + availableRocks[i]);
+                }
+            }
+            rockNumber = scanner.nextInt();
+            Move.DoMove(availableRocks[rockNumber - 1], board, diceResult);
+        } else {
+            for (int i = 0; i < availableRocks.length; i++) {
+                if (!availableRocks[i].finish && availableRocks[i].getPosition() != -1) {
+                    System.out.println("Rock " + (i + 1) + ": Position " + availableRocks[i]);
+                }
+            }
+            rockNumber = scanner.nextInt();
+            Move.DoMove(availableRocks[rockNumber - 1], board, diceResult);
+        }
+        boolean x = Rules.kill(board, availableRocks[rockNumber - 1], otherPlayer.getPlayRocks());
+        board.printBoard();
+    }
+
+    private void getComputerMove() {
+//        System.out.println("kkk");
+        DiceRolls rand2 = new DiceRolls();
+        rand2.rollDice();
+
+        Player user = turn ? player1 : player2;
+        Player computer = turn ? player2 : player1;
+
+
+        if (computer.hasWon(computer.getPlayRocks()))
+        {
+            scanner.close();
+            System.out.println("sahozy : " + computer.getName() + " has won the game!");
+            // break;
+        }
+        handleDiceRollAndMoveForComputer(user, computer, rand2);
+
+
+
+    }
+
+    public void runForOnePlayer(){
+        while (true){
+            System.out.println("___________________"+player1.getName()+"___________________");
+//            getUserMove();
+            DiceRolls rand1 = new DiceRolls();
+            rand1.rollDice();
+
+            Player user = turn ? player1 : player2;
+            Player computer = turn ? player2 : player1;
+
+
+            if (user.hasWon(user.getPlayRocks()))
+            {
+                scanner.close();
+                System.out.println("sahozy : " + user.getName() + " has won the game!");
+                break;
+            }
+
+            System.out.println("sahozy : Press 1 to roll the dice or 0 to exit : player : " + user.getName());
+            int input = scanner.nextInt();
+
+            if (input == 0) {
+                break;
+            } else if (input == 1) {
+                handleDiceRollAndMove(user, computer, rand1);
+            } else {
+                System.out.println("sahozy : Invalid input!");
+            }
+            turn = !turn;
+            if(turn = !turn){
+                System.out.println("___________________Computer___________________");
+                getComputerMove();
+            }
+
+
+
+        }}
+
 }
